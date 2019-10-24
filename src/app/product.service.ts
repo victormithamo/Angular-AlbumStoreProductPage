bbf2e0f7dcc9dc2a70e8ecd822afbe9c7cdd61ff
victor <vickarungaru@gmail.com>
2019-10-24 13:34:05 +0300

    fixed product.service bug

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  private _albumUrl: '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
